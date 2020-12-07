489013e0a98fceaee36917537090dcad5c996ccc
Yuri Miranda Fialho <mirandafialho@gmail.com>
2020-12-06 21:14:57 -0300

    Corrigida a instalação do Vue.js

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,51 +1,12 @@
 <template>
   <div id="app">
-    <NavBar/>
-    <FullCalendar :options="calendarOptions" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import NavBar from './components/Layout/NavBar.vue'
-
-// Fullcalendar
-import FullCalendar from '@fullcalendar/vue'
-import listPlugin from '@fullcalendar/list'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
-
-export default {
-  name: 'App',
-  components: {
-    NavBar,
-    FullCalendar,
-  },
-  data() {
-    return {
-      calendarOptions: {
-        plugins: [ listPlugin, dayGridPlugin, timeGridPlugin, interactionPlugin ],
-        initialView: 'dayGridMonth',
-        dateClick: this.handleDateClick,
-        headerToolbar: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay'
-        },
-        events: [
-          { title: 'event 1', date: '2020-12-01' },
-          { title: 'event 2', date: '2020-12-02' }
-        ]
-      }
-    }
-  },
-  methods: {
-    handleDateClick: function(arg) {
-      alert('date click! ' + arg.dateStr)
-    }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -54,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 0px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,27 +1,13 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import App from './App.vue'
+import './registerServiceWorker'
 import router from './router'
-
-// Import the styles directly. (Or you could add them via script tags.)
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-// Install BootstrapVue
-Vue.use(BootstrapVue)
-
-// This imports <b-modal> as well as the v-b-modal directive as a plugin:
-import { ModalPlugin } from 'bootstrap-vue'
-Vue.use(ModalPlugin)
-
-// This imports the dropdown and table plugins
-import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
-Vue.use(DropdownPlugin)
-Vue.use(TablePlugin)
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-	router,
-	render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
