39cb6d0fde1e28a7bcdd18b7bc4cafc96d24b02e
Yuri Miranda Fialho <mirandafialho@gmail.com>
2020-12-09 07:49:55 -0300

    Login de usuários

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="login">
+        <b-form @submit="login" v-if="show">
+            <b-form-group
+                id="input-group-1"
+                label="E-mail:"
+                label-for="email"
+            >
+                <b-form-input
+                    id="user-email"
+                    v-model="email"
+                    type="email"
+                    required
+                ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                id="input-group-2"
+                label="Senha:"
+                label-for="senha"
+            >
+                <b-form-input
+                    id="user-password"
+                    v-model="password"
+                    type="password"
+                    required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-button-group>
+                <b-button type="submit" variant="success">Entrar</b-button>
+            </b-button-group>
+        </b-form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            email: '',
+            password: '',
+            show: true
+        }
+    },
+    methods: {
+        login() {
+            console.log(this)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
